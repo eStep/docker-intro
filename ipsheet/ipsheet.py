b49981e2aff3578d6f138ed22664cb56f86a84d2
mkuzak <mateusz.kuzak@gmail.com>
2015-09-09 13:23:56 +0200

    Make script talking to spreadsheet work

--- ipsheet/ipsheet.py
+++ ipsheet/ipsheet.py
@@ -5,7 +5,7 @@ import threading
 from oauth2client.client import SignedJwtAssertionCredentials
 
 # connect to spreadsheet
-json_key = json.load(open('~/Downloads/ipsheet-e54e6f9a4d37.json'))
+json_key = json.load(open('/Users/mkuzak/Downloads/ipsheet-e54e6f9a4d37.json'))
 scope = ['https://spreadsheets.google.com/feeds']
 
 credentials = SignedJwtAssertionCredentials(json_key['client_email'],
@@ -16,7 +16,7 @@ gc = gspread.authorize(credentials)
 wks = gc.open("ipsheet").sheet1
 
 # get ip address
-eth0 = ni.ifaddresses('eth0')
+eth0 = ni.ifaddresses('lo0')
 ip_address = eth0[2][0]['addr']
 
 # check if this ip address is already in the spreadsheet,
@@ -26,16 +26,19 @@ try:
     # this is fine ip adress is already there
 except:
     # append the address
-    wks.append_row([ip_address])
+    wks.update_cell(len(wks.col_values(1)) + 1, 1, ip_address)
 
 # need to querry spreadsheet(for example every 10s)
 # when key is pasted pick it up and stop queries
 def fetch_key():
-    cell = cell.find(ip_address)
-    key = wks.acell(cell.row, 2)
+    cell = wks.find(ip_address)
+    key = wks.cell(cell.row, 2).value
     if (key == ""):
         # querry again
-        threading.Timer(0.5, fetch_key)
+        print("waiting for input")
+        threading.Timer(0.5, fetch_key).start()
     else:
         # add the key
         print(key)
+
+fetch_key()
